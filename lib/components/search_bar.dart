6a394548202f2561e921d7a026ad7d8dc171cb63
Gap08 <cassylum23@gmail.com>
2023-05-24 11:25:17 +0800

    added the filter button and search

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -10,6 +10,9 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
+
+  Color $whitesh = const Color(0xffF5F4F7);
+  Color $mustard = const Color(0xffF4C900);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,11 +48,11 @@ class _SearchBarState extends State<SearchBar> {
                 Stack(
                   children: [
                     Neumorphic(
-                      style: const NeumorphicStyle(
+                      style: NeumorphicStyle(
                         shape: NeumorphicShape.flat,
-                        depth: -4,
+                        depth: -3.5,
                         intensity: 0.8,
-                        color: Colors.white
+                        color: $whitesh
                       ),
                       child: Container(
                         width: 325,
@@ -69,11 +72,92 @@ class _SearchBarState extends State<SearchBar> {
                           )
                       ),
                     ),
-                    // NeumorphicButton(
-                    //   child: const Icon(
-                    //     Iconsax.filter,
-                    //   ),
-                    // )
+                    Positioned(
+                      top: 10,
+                      right: 35,
+                        child: Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: [
+                            NeumorphicButton(
+                            padding: EdgeInsets.zero,
+                            style: NeumorphicStyle(
+                              color: Colors.lightBlue,
+                              shape: NeumorphicShape.flat,
+                              depth: 2,
+                              intensity: 1,
+                              boxShape: NeumorphicBoxShape.roundRect(
+                                BorderRadius.circular(5)
+                                )
+                            ),
+                            child: SizedBox(
+                              width: 21.75,
+                              height: 21.75,
+                              // child: Icon(
+                              // Iconsax.filter_search,
+                              // size: 20.75,
+                              // color: $mustard,
+                              // ),
+                              child: Text(
+                                String.fromCharCode(Iconsax.filter_search.codePoint),
+                                textAlign: TextAlign.center,
+
+                                style: TextStyle(
+                                  inherit: false,
+                                  color: $mustard,
+                                  fontSize: 18.75,
+                                  fontWeight: FontWeight.w900,
+                                  fontFamily: Iconsax.filter_search.fontFamily,
+                                  package: Iconsax.filter_search.fontPackage,
+                                ),
+                              )
+                            )
+                            ),
+                          ]
+                        ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                        child: Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: [
+                            NeumorphicButton(
+                            padding: EdgeInsets.zero,
+                            style: NeumorphicStyle(
+                              color: Colors.lightBlue,
+                              shape: NeumorphicShape.flat,
+                              depth: 2,
+                              intensity: 1,
+                              boxShape: NeumorphicBoxShape.roundRect(
+                                BorderRadius.circular(5)
+                                )
+                            ),
+                            child: SizedBox(
+                              width: 21.75,
+                              height: 21.75,
+                              // child: Icon(
+                              // Iconsax.filter_search,
+                              // size: 20.75,
+                              // color: $mustard,
+                              // ),
+                              child: Text(
+                                String.fromCharCode(Iconsax.search_normal.codePoint),
+                                textAlign: TextAlign.center,
+
+                                style: TextStyle(
+                                  inherit: false,
+                                  color: $mustard,
+                                  fontSize: 18.75,
+                                  fontWeight: FontWeight.w900,
+                                  fontFamily: Iconsax.search_normal.fontFamily,
+                                  package: Iconsax.search_normal.fontPackage,
+                                ),
+                              )
+                            )
+                            ),
+                          ]
+                        ),
+                    )
                   ],
                 ),
               ],
