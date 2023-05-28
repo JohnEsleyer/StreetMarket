dd30a36d7a223aefdda691c33291f98029a47dff
john <ralphesleyer@gmail.com>
2023-05-28 15:17:40 +0800

    added code for search_mode_sample.dart

--- lib/models/PostDataModel.dart
+++ lib/models/PostDataModel.dart
@@ -1,9 +1,40 @@
-// class PostDataModel {
-//   final String? title;
-//   final String? imageUrl;
-//   final String? location;
-//   final int? minutes;
-//   final int? month;
-//   final String? price;
-//   final username 
-// }
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class PostDataModel {
+  final String? title;
+  final String? imageUrl;
+  final String? location;
+  final int? minutes;
+  final int? month;
+  final int? year;
+  final String? price;
+  final String? username;
+
+  PostDataModel(
+      {this.title,
+      this.imageUrl,
+      this.location,
+      this.minutes,
+      this.year,
+      this.month,
+      this.price,
+      this.username});
+
+  // QuerySnapshot from Cloud Firestore to a list of objects of this DataModel
+
+  List<PostDataModel> dataListFromSnapshot(QuerySnapshot querySnapshot) {
+    return querySnapshot.docs.map((snapshot) {
+      final Map<String, dynamic> dataMap =
+          snapshot.data() as Map<String, dynamic>;
+
+      return PostDataModel(
+        title: dataMap['title'],
+        imageUrl: dataMap['imageUrl'],
+        location: dataMap['location'],
+        minutes: dataMap['minutes'],
+        month: dataMap['month'],
+        year: dataMap['year'],
+      );
+    }).toList();
+  }
+}
--- lib/samples/search_mode.dart
+++ lib/samples/search_mode.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firestore_search_input/firestore_search_input.dart';
+import 'package:streetmarket/models/PostDataModel.dart';
 
 import '../models/UserData.dart';
 
@@ -52,27 +54,60 @@ class _SearchModeScreenState extends State<SearchModeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Color.fromARGB(255, 222, 182, 0),
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Hero(
-              tag: 'search',
-              child: Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Text("Search Mode"),
+    return Expanded(
+      child: FirestoreSearchScaffold(
+        firestoreCollectionName: 'posts',
+        searchBy: 'title',
+        dataListFromSnapshot: PostDataModel().dataListFromSnapshot,
+        scaffoldBody: Center(),
+        builder: (context, snapshot) {
+          print(snapshot.error);
+
+          if (snapshot.hasData) {
+            final List<PostDataModel>? dataList = snapshot.data;
+            if (dataList!.isEmpty) {
+              return const Center(
+                child: Text('No Results Found!'),
+              );
+            }
+            return ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                final PostDataModel data = dataList[index];
+
+                return Container(
+                  child: Text(data.title as String),
+                );
+              },
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     automaticallyImplyLeading: false,
+    //     backgroundColor: Color.fromARGB(255, 222, 182, 0),
+    //     centerTitle: true,
+    //     title: Row(
+    //       mainAxisAlignment: MainAxisAlignment.end,
+    //       children: [
+    //         Hero(
+    //           tag: 'search',
+    //           child: Icon(
+    //             Icons.search,
+    //             color: Colors.black,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    //   body: Center(
+    //     child: Text("Search Mode"),
+    //   ),
+    // );
   }
 }
